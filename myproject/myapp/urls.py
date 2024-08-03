d8249422cd159d4d30cafdddae998df020c7a0ee
SUJAN <pokharelisujan@gmail.com>
2024-08-04 00:34:26 +0900

    Initial commit with project setup

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('book/<int:pk>/', views.book_detail, name='book_detail'),
+    path('book/new/', views.book_new, name='book_new'),
+    path('book/<int:pk>/edit/', views.book_edit, name='book_edit'),
+    path('book/<int:pk>/delete/', views.book_delete, name='book_delete'),
+]
